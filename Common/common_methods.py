ab1666c718ff82aeb94e13c0981e21ba95f17bea
lucasgsfelix <lucasgsfelix@gmail.com>
2019-04-09 21:12:40 -0300

    Add files via upload

--- Common/common_methods.py
+++ Common/common_methods.py
@@ -1,5 +1,10 @@
 """Every class will have acess to this"""
+import math
 
 class Common_Methods():
 
-	def dinamyc_alfa(data, forecast_data):
+	def lead_time_calc(lead_value, forecast_line):
+		
+		lead_value = math.floor(lead_value/30)
+		added_values = [0 for i in lead_value]
+		lead_value = added_values + lead_value
--- Forecast_Matrix/croston_based.py
+++ Forecast_Matrix/croston_based.py
@@ -12,29 +12,31 @@ def calculate_method_exponential(alfa, data_value, forecast_value):
 
 class Croston_Based():
 
-	alfa, convergence_value, percentile = 0.1,100,10 ## class atributes
+	alfa, beta, convergence_value, percentile = 0.1, 0.1,100,10 ## class atributes
 	choosen_algorithm, alfa_condition = 'croston', 'fix'
 	bootstrap_call = False
 	data = pd.DataFrame()
 
-	def croston_main(self, alfa_condition, alfa, data, choosen_algorithm, bootstrap_call, convergence_value, percentile):
+	def croston_main(self, alfa_condition=None, alfa=None, beta=None, data, choosen_algorithm=None, 
+							bootstrap_call=None, convergence_value=None, percentile=None):
 		"""Call and defines methods
 			alfa_condition: fix ou dynamic
 			alfa: most be in a range of 0 to 1
 			data: the data most be a pandas dataframe
-			choosen_algorithm: sba, croston or exponential
+			choosen_algorithm: sba, croston, tsb or exponential
 			bootstrap_call: if true call boostrap, but does not work if exponential
 			convergence_value: the value of convergence for the bootstrap method, most be in greater than 0
 			percentile: defines the percentile for the bootstrap method, most be between 0 ~ 100
 		"""
 		self.alfa = alfa
+		self.beta = beta
 		self.data = data
 		self.alfa_condition = alfa_condition
 		self.choosen_algorithm = choosen_algorithm
 		self.bootstrap_call = bootstrap_call
 		self.convergence_value = convergence_value
 		self.percentile = percentile
-		forecast_matriz = np.zeros(shape=data.shape, dtype=np.float64)
+		forecast_matrix = np.zeros(shape=data.shape, dtype=np.float64)
 		
 		for_initial_pass = 0
 		if choosen_algorithm.lower() == 'exponential':
@@ -52,18 +54,29 @@ class Croston_Based():
 					if bootstrap_call == True:
 						bootstrap_obj.bootstrap_main_init(row = data[i][0:j])
 					else:
-						if choosen_algorithm.lower() != 'exponential':
+						if choosen_algorithm.lower() != 'exponential' and choosen_algorithm.lower() != 'tsb':
 							previous_p = actual_p
 							previous_z = actual_z
 							value_q = value_q + 1
+
+						elif choosen_algorithm.lower() == 'tsb':
+							actual_p = previous_p + beta * (0 - previous_p)
+							actual_z = previous_z
+							forecast_matrix[i][j] = actual_z*actual_z
+
 				else:
 					if choosen_algorithm.lower() == 'exponential':
-						forecast_matriz[i][j] = calculate_method_exponential(alfa, data[i][j], forecast_value[i][j-1])
+						forecast_matrix[i][j] = calculate_method_exponential(alfa, data[i][j], forecast_value[i][j-1])
+					elif choosen_algorithm.lower() == 'tsb':
+						forecast_matrix[i][j] = tsb_method(data_value, alfa, beta)
 					else:
-						forecast_matriz[i][j] = self.calculate_method(data[i][j], alfa, value_q)
+						forecast_matrix[i][j] = self.calculate_method(data[i][j], alfa, value_q)
 
 					if self.alfa_condition.lower() == 'dynamic':
-						alfa = self.dynamic_alfa(data[i][j], value_q, forecast_matriz[i][j-1]) ## choosing the best value for alfa
+						if choosen_algorithm.lower() == 'tsb':
+							alfa, beta = dynamic_alfa_beta(data_value)
+						else:
+							alfa = self.dynamic_alfa(data[i][j], value_q, forecast_matrix[i][j-1]) ## choosing the best value for alfa
 					value_q = 1
 
 	def calculate_method_croston_based(self, data_value, alfa, value_q):
@@ -79,6 +92,13 @@ class Croston_Based():
 			forecast_value = (1.0 - (alfa/2))*forecast_value ## sba method
 
 		return forecast_value
+
+	def tsb_method(self, data_value, alfa, beta):
+		"""TSB method implementation"""
+		actual_p = previous_p + beta * (1-previous_p)
+		actual_z = previous_z + alfa * (data_value - previous_z)
+		
+		return actual_p * actual_z
 	
 	def dynamic_alfa(self, value_q, data_value, forecast_value):
 		"""Calculates the dynamic alfa value"""
@@ -101,10 +121,29 @@ class Croston_Based():
 
 		return smaller_alfa
 
-'''
-if __name__ == '__main__':
-	
-	sis = Croston_Based()
-	sis.croston_main(0,0,0,0,0,0,0)
+	def dynamic_alfa_beta(data_value):
 
-'''
+		alfa, beta, flag = 0, 0, 0
+		best_alfa, best_beta = 0, 0
+		while(alfa<=1):
+			beta = 0
+			while(beta<=1):
+				predicted_value = tsb_method(data_value, alfa, beta)
+				error_value = error.simple_error(data_value, dynamic_value)
+				
+				if flag == 0:	
+					aux = error_value
+					best_beta = beta
+					best_alfa = alfa
+					flag = 1
+				
+				else:
+					if aux>error_value:
+						error_value = aux
+						best_beta = beta
+						best_alfa = alfa
+
+				beta = beta + 0.1
+			alfa = alfa + 0.1
+
+		return alfa, beta
